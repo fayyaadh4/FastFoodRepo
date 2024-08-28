d5b37edb15241c44e270518748a1d93a376f1b44
Fayyaadh <fayyaadh.naidoo@gmail.com>
2024-08-28 23:54:06 +0200

    Set [Required] on Name fields on Restaurant and Menu Item Dto

--- FastFood/Dto/MenuItemDto.cs
+++ FastFood/Dto/MenuItemDto.cs
@@ -1,8 +1,11 @@
-﻿namespace FastFood.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Dto
 {
     public class MenuItemDto
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
--- FastFood/Dto/RestaurantDto.cs
+++ FastFood/Dto/RestaurantDto.cs
@@ -1,10 +1,12 @@
 ﻿using FastFood.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace FastFood.Dto
 {
     public class RestaurantDto
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? Category { get; set; }
